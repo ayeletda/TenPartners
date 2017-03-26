c6ea7be75f50b944912826d5bbb7fb02751f6512
Adva Avraham Cahana <advaav55@gmail.com>
2017-03-26 09:40:01 +0300

    login

--- TenPartnersApp/src/app/app.module.ts
+++ TenPartnersApp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
