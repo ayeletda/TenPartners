b53b1bae7626ff35f3e14d835e12e7b22b52f449
Adva Avraham Cahana <advaav55@gmail.com>
2017-03-26 15:30:07 +0300

    login

--- TenPartnersApp/src/app/app.component.ts
+++ TenPartnersApp/src/app/app.component.ts
@@ -6,14 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username:string = '';
-  password:string = '';
-
-clicked(){
-this.username =null;
-this.password = null;
-}
-
+ 
 
 
 }
---
+++ TenPartnersApp/src/app/main/main.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+   username:string = '';
+  password:string = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+
+clicked(username:HTMLInputElement, password:HTMLInputElement){
+  
+if(username.value=="advaav55@gmail.com"&&password.value=="1234")
+    alert("Wellcom tenPartner")
+
+password.value=null;
+username.value=null;
+}
+
+
+}
