476ac7a8c30a34134c0ceaeb1742685ef391fa6c
rachelies <racheli@gmail.com>
2017-04-26 20:00:51 +0300

    add board to router

--- TenPartnersApp/src/app/routes.ts
+++ TenPartnersApp/src/app/routes.ts
@@ -2,12 +2,14 @@ import {Routes} from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { BoardComponent } from './pages/board/board.component';
 
 
 export const appRoutes: Routes=[
 {path:'', component: LoginComponent},
 {path:'home', component: HomeComponent},
 {path:'settings', component: SettingsComponent},
+{path:'board', component: BoardComponent}
 
 
 
