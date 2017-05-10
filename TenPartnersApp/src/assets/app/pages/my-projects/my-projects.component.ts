ad183c0106ad572b6362bb74e0776bb42673529f
Hadas135 <hadasi135@gmail.com>
2017-05-10 11:29:47 +0300

    update my-projects

--- TenPartnersApp/src/assets/app/pages/my-projects/my-projects.component.ts
+++ TenPartnersApp/src/assets/app/pages/my-projects/my-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-projects',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  
+  clickOnMyProjects(event){
+    this.router.navigateByUrl('/'+event.currentTarget.id);
   }
 
 }
