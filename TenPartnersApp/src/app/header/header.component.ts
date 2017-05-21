c93ac65f5e9406b3b4b96dac75f587dbe757fa71
Adva Avraham Cahana <advaav55@gmail.com>
2017-05-21 11:02:23 +0300

    title

--- TenPartnersApp/src/app/header/header.component.ts
+++ TenPartnersApp/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component
@@ -12,9 +12,13 @@ export class HeaderComponent implements OnInit
 {
   constructor(private router: Router) { }
 
+  @Input()item;
+
   ngOnInit() {}
   
   logOut(){
+    console.log("home");
+
     this.router.navigateByUrl('');
   }
 
