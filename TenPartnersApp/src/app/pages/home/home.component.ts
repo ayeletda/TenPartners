9d7d186b80d365691bee93b669b66242f3238107
Hadas135 <hadasi135@gmail.com>
2017-05-29 19:34:45 +0300

    Adding examples to "home"

--- TenPartnersApp/src/app/pages/home/home.component.ts
+++ TenPartnersApp/src/app/pages/home/home.component.ts
@@ -23,3 +23,44 @@ export class HomeComponent implements OnInit {
 
 
 }
+
+/*
+
+What we do with Yonatan:
+on SubmitProjectComponent.ts:
+
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AfterViewChecked, ElementRef, ViewChild, Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { ChangeDetectorRef } from "@angular/core";
+
+
+@Component({
+  selector: 'app-submit-project',
+  templateUrl: './submit-project.component.html',
+  styleUrls: ['./submit-project.component.css']
+})
+export class SubmitProjectComponent implements OnInit {
+public projects: FirebaseListObservable<any>;
+public newProject: string;
+  constructor(private router: Router, public af: AngularFireDatabase)
+   {
+  this.projects = this.af.list('projects'); //= select * from projects 
+
+    this.projects.push({game:{goal:'great'}}); //= select * from projects 
+ this.projects.lift
+    //this.newProject = '';
+    }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
+
+
+*/
