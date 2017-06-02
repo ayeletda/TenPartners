272630ef9d9d10f49ecd6186a2bf5aee526a5fda
rachelies <racheli@sacks.family>
2017-06-02 16:58:41 +0300

    board changes for design

--- TenPartnersApp/src/app/pages/board/board.component.ts
+++ TenPartnersApp/src/app/pages/board/board.component.ts
@@ -3,6 +3,129 @@ import {Router} from '@angular/router';
 import {ServiceService} from '../../service.service';
 
 
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
+import { ChangeDetectorRef } from "@angular/core";
+
+
+@Component(
+    {
+        selector: 'app-board',
+        templateUrl: './board.component.html',
+        styleUrls: ['./board.component.css']
+    })
+
+
+export class BoardComponent implements OnInit {
+    savedDate: string = '';
+    user: FirebaseListObservable<any>;
+    projects: FirebaseListObservable<any>;
+    associatedCommunities: FirebaseListObservable<any>;
+    userId: string;
+    userCommunity: string;
+    currentProject: any = '';
+    currentProjectValues: any = '';
+    projectPath: any = '';
+    projectSelected:boolean;
+    constructor(private router: Router,private service:ServiceService, public af: AngularFireDatabase) {
+
+        this.userId = this.service.getCurrentID();
+        this.user = this.af.list('users/' + this.userId); // the specific user
+
+        this.user.subscribe((snapshots)=>{
+            snapshots.forEach(snapshot => {
+                if (snapshot.$key == 'associatedCommunity')
+                    this.userCommunity = snapshot.$value;
+
+            });
+        })
+        this.projects = this.af.list('projects');
+
+        this.projects.subscribe((snapshots)=>{
+            snapshots.forEach(snapshot => {
+                // array.push(this.af.list('projects/' + snapshot.$key + '/associatedCommunities'));
+                // array2.push(snapshot);
+                this.associatedCommunities = this.af.list('projects/' + snapshot.$key + '/associatedCommunities');
+                this.currentProjectValues = snapshot;
+
+            });
+        })
+        this.projectSelected = false;
+
+    }
+
+    ngOnInit() {
+
+
+        this.service.setTitle("Suggested projects");
+
+        (<any>$('#SuggestedProjects')).slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false
+        });
+    }
+    saveProjectPath(project)
+    {
+        this.projectPath = 'projects/' + this.currentProjectValues.$key + '/associatedCommunities/' + project.$key;
+        return true;
+    }
+
+    loadProjectDetails(project)
+    {
+        this.currentProject = project;
+        this.projectSelected = false;
+    }
+    updateAssociatedUser(project){
+        this.projectSelected = true;
+        this.currentProject = project;
+        this.currentProject.update({'associatedUser': this.currentProject.userID});
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ServiceService} from '../../service.service';
+
+
 @Component(
     {
         selector: 'app-board',
@@ -34,4 +157,4 @@ export class BoardComponent implements OnInit {
 
     }
 }
- 
+ */
--- TenPartnersApp/src/app/projectForSelect/projectForSelect.component.ts
+++ TenPartnersApp/src/app/projectForSelect/projectForSelect.component.ts
@@ -1,3 +1,57 @@
+
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AfterViewChecked, ElementRef, ViewChild, Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { ChangeDetectorRef, Input } from "@angular/core";
+
+
+@Component(
+    {
+      selector: 'app-projectForSelect',
+      templateUrl: './projectForSelect.component.html',
+      styleUrls: ['./projectForSelect.component.css']
+    })
+
+export class ProjectForSelectComponent implements OnInit
+{
+  @Input() item;
+  private projects: FirebaseListObservable<any>;
+  private pointerToProjectInAF: any;
+  pointerToProjectObjectInAF:any;
+  projectName: string;
+
+  constructor(private router: Router, private af: AngularFireDatabase)
+  {
+    this.projects=this.af.list('projects');
+  }
+
+  ngOnInit() {
+
+    this.pointerToProjectInAF =  this.af.list(this.item); // item is a path
+    this.pointerToProjectObjectInAF =  this.af.object(this.item ,{ preserveSnapshot: true });
+    this.projectName=this.pointerToProjectInAF.$ref.path.o[1]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -14,3 +68,4 @@ export class ProjectForSelectComponent implements OnInit
 
   ngOnInit() {}
 }
+    */
