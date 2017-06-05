c7ddba772115c56d2c9360d63c44a9eaea0a57f6
Adva Avraham Cahana <advaav55@gmail.com>
2017-06-06 00:14:51 +0300

    submited

--- TenPartnersApp/src/app/dbproject/dbproject.component.ts
+++ TenPartnersApp/src/app/dbproject/dbproject.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AfterViewChecked, ElementRef, ViewChild, Component, OnInit, EventEmitter } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { ChangeDetectorRef, Input, Output } from "@angular/core";
 
 @Component({
   selector: 'app-dbproject',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dbproject.component.css']
 })
 export class DBprojectComponent implements OnInit {
+  public Name:String;
+  public Description:String;
+  public Purpose:String;
+  private projects: FirebaseListObservable<any>;
+  private pointerToProjectInAF: any;
+  pointerToProjectObjectInAF: any;
+  @Input() item;
 
-  constructor() { }
+
+  constructor(private router: Router, private af: AngularFireDatabase) { 
+        this.projects = this.af.list('projects');
+
+  }
 
   ngOnInit() {
+    this.pointerToProjectInAF = this.af.list(this.item); // item is a path
+    this.pointerToProjectObjectInAF = this.af.object(this.item, { preserveSnapshot: true });
+    this.Name = this.pointerToProjectInAF.$ref.path.o[1];
+
   }
 
 }
--- TenPartnersApp/src/app/pages/submit-project/submit-project.component.ts
+++ TenPartnersApp/src/app/pages/submit-project/submit-project.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {ServiceService} from '../../service.service';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AfterViewChecked, ElementRef, ViewChild, Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
+import { ChangeDetectorRef } from "@angular/core";
+import { ServiceService } from '../../service.service';
+
+
 
 @Component({
   selector: 'app-submit-project',
@@ -8,8 +15,46 @@ import {ServiceService} from '../../service.service';
 })
 export class SubmitProjectComponent implements OnInit {
 
-  constructor(private serviceService:ServiceService) { }
+  public Name:String;
+  public Description:String;
+  public Purpose:String;
+  projects: FirebaseListObservable<any>;
+  projectsValues_Arr: any;
+  public messages: FirebaseListObservable<any>;
 
-  ngOnInit() {this.serviceService.setTitle("Submit Project");}
+
+  constructor(private service: ServiceService, private router: Router, public af: AngularFireDatabase) 
+  {
+    this.Name="";
+    this.Description="";
+    this.Purpose="";
+
+     this.projects = this.af.list('projects');
+
+    this.projects.subscribe((snapshots)=>{
+              this.projectsValues_Arr=[];
+      snapshots.forEach(snapshot => {
+   //////////// mybe in oninit
+        this.projectsValues_Arr.push(snapshot);});})
+
+  }
+
+  ngOnInit() {this.service.setTitle("Submit Project");}
+
+  sendProject()
+  {
+    console.log("hrhr "+ this.Name + this.Description+this.Purpose);
+
+    if(this.Name==""||this.Description==""||this.Purpose=="")
+        alert("something's missing");
+
+    else {
+      this.projects.push({Name: this.Name, Description: this.Description,Purpose: this.Purpose});
+      this.Name = '';
+      this.Description="";
+      this.Purpose="";
+       alert("your project send!");
+      }
+  }
 
 }
