5e8d0ce2d326c82290f8b33a78a3eb761458a8bb
Avia-Cheriker <aviacheriker@gmail.com>
2017-06-05 15:05:45 +0300

    bdika arik

--- TenPartnersApp/src/app/header/header.component.ts
+++ TenPartnersApp/src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import {ServiceService} from '../service.service';
 
-
+//stam bdika
 
 @Component
 ({
