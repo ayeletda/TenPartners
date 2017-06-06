514a3517abee6afc7ea06ff7221500580afba8fc
Adva Avraham Cahana <advaav55@gmail.com>
2017-06-06 18:44:30 +0300

    NN

--- TenPartnersApp/src/app/pages/submit-project/submit-project.component.ts
+++ TenPartnersApp/src/app/pages/submit-project/submit-project.component.ts
@@ -49,7 +49,7 @@ export class SubmitProjectComponent implements OnInit {
         alert("something's missing");
 
     else {
-      this.projects.push({Name: this.Name, Description: this.Description,Purpose: this.Purpose,associatedCommunities: null});
+      this.projects.push({Name: this.Name, Description: this.Description,Purpose: this.Purpose,associatedCommunities: "NULL"});
       this.Name = '';
       this.Description="";
       this.Purpose="";
