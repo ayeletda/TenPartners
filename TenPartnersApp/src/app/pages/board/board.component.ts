bac8057d2975c66d34119a3e8fa61d78db90b88b
Avia-Cheriker <aviacheriker@gmail.com>
2017-06-18 03:00:53 +0300

    board - ordering & fixing according to new service

--- TenPartnersApp/src/app/pages/board/board.component.ts
+++ TenPartnersApp/src/app/pages/board/board.component.ts
@@ -6,120 +6,111 @@ import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} f
 import {ChangeDetectorRef,Input} from "@angular/core";
 import {ServiceService} from '../../service.service';
 
-
-
 @Component(
-    {
-        selector: 'app-board',
-        templateUrl: './board.component.html',
-        styleUrls: ['./board.component.css']
-    })
+{
+    selector: 'app-board',
+    templateUrl: './board.component.html',
+    styleUrls: ['./board.component.css']
+})
 
-export class BoardComponent implements OnInit {
+//====================  Board class  ============================================================
 
-    @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+export class BoardComponent implements OnInit 
+{
     @Input()path;
+    @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
+    //user's details
+    private user = { id: null, permission: null, community: null, name: null, email: null };
 
-    // variables
-    private userId: string;
-    private userCommunity: string;
-    private name: string;
-    private email: string;
-
+    //project's details
     private currentProject: any;
     private currentI: any;
     private projectPath: any;
-    private projectSelected: boolean;
     private cost: number;
     private date: Date;
     private purpose: string;
     private description: string;
 
-    private showDetailsForm: boolean;
-    private noProjects:boolean;
-
-
-    private firstTimeOfScoller: boolean;
-
     private savedDate: string;
     private newMessage: string;
     private projectUpdate: FirebaseObjectObservable<any>;
 
+    //determined according to customer
+    private maxVotingNum: number;    
+
     // pointers of object or list in firebase
-    private user: FirebaseListObservable<any>; // pointer to user
     private projects: FirebaseListObservable<any>;
     private projectsAssociatedCommunities_Arr: any;
     private projectsValues_Arr: any;
     private needViewMore: boolean;
-  private usersVotingList: FirebaseListObservable<any>;
+    private usersVotingList: FirebaseListObservable<any>;
     private pointerToProjectObjectInAF: FirebaseObjectObservable<any>;
 
+    //flags
+    private showDetailsForm: boolean;
+    private noProjects:boolean;
+    private projectSelected: boolean;
+    private firstTimeOfScoller: boolean;
 
 
-//======================================================  constructor  ============================================================
+    //====================  constructor  ============================================================
 
-    constructor(private router: Router, private service: ServiceService, public af: AngularFireDatabase) {
-        this.userId = this.service.getCurrentID();
-        this.user = this.af.list('users/' + this.userId); // the specific user
-        this.name = this.service.getCurrentUser();
-        this.email = this.service.getCurrentEmail();
-        this.firstTimeOfScoller = true;
-        this.showDetailsForm = false;
-        this.noProjects= true;
-
-        let temp = this.user.subscribe((snapshots) => {
-            snapshots.forEach(snapshot => {
-                if (snapshot.$key == 'associatedCommunity')
-                    this.userCommunity = snapshot.$value;
-            });
-        });
-
-        this.service.allSubscribe.push(temp);
-
-        this.projects = this.af.list('projects');
-
-        let temp2 = this.projects.subscribe((snapshots) => {
-            this.projectsAssociatedCommunities_Arr = [];
-            this.projectsValues_Arr = [];
-
-            snapshots.forEach(snapshot => {
-                this.projectsAssociatedCommunities_Arr.push(this.af.list('projects/' + snapshot.$key + '/associatedCommunities'));
-                this.projectsValues_Arr.push(snapshot);
-            });
-        });
-
-       this.service.allSubscribe.push(temp2);
-
-
+    constructor(private router: Router, private service: ServiceService, public af: AngularFireDatabase) 
+    {    
+        //initializes with defult values
         this.newMessage = '';
         this.savedDate = '';
         this.currentProject = '';
         this.projectPath = '';
         this.projectSelected = false;
+        this.firstTimeOfScoller = true;
+        this.showDetailsForm = false;
+        this.noProjects= true;
+        this.maxVotingNum = 10;
+
+        //function (in servic.component.ts) that includs subscribe that listen to firebase and initializes the variabels: userId, userCommunity, name, email 
+        this.service.getDetails(this.user);
+
+        //initialize arrays
+        this.projects = this.af.list('projects');
+
+        let temp2 = this.projects.subscribe((snapshots) => 
+        {
+            this.projectsAssociatedCommunities_Arr = [];
+            this.projectsValues_Arr = [];
+
+            snapshots.forEach(snapshot => 
+            {
+                this.projectsAssociatedCommunities_Arr.push(this.af.list('projects/' + snapshot.$key + '/associatedCommunities'));
+                this.projectsValues_Arr.push(snapshot);
+            });
+        });
+
+        //pushes subscribe to an array for freeing it (listener to firebase) when login-out
+       this.service.allSubscribe.push(temp2);
     }
 
-//========================================================  ngOnInit  ============================================================
+    //===================  ngOnInit  ============================================================
 
-    ngOnInit() {
+    ngOnInit() 
+    {
         this.scrollToBottom()
         this.service.setTitle("Submitted Projects");
-    
-
     }
 
+    //================  saveProjectPath  =========================================================
 
-//========================================================  saveProjectPath  =========================================================
-
-    saveProjectPath(project, i) {
+    private saveProjectPath(project, i) 
+    {
         this.projectPath = 'projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/' + project.$key;
         this.noProjects = false;
         return true;
     }
 
-//======================================================  loadProjectDetails  =========================================================
+    //===============  loadProjectDetails  =========================================================
 
-    loadProjectDetails(project, i) 
+    private loadProjectDetails(project, i) 
     {
         this.currentProject = project;
         this.currentI = i;
@@ -135,51 +126,58 @@ export class BoardComponent implements OnInit {
         this.usersVotingList = this.af.list('projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/'+project.$key + "/votingList");
     }
 
-    chooseProject()
+    //==================  chooseProject  ============================================================
+
+    private chooseProject()
     {
         this.showDetailsForm = true;
     }
 
-    updateDetails() 
+    private updateDetails() 
     {
         //updating project's details
-        this.projectUpdate.update({'associatedUser': this.userId });
+        this.projectUpdate.update({'associatedUser': this.user.id });
         this.projectUpdate.update({ 'uploudDate': new Date().getTime() });
         this.projectUpdate.update({ 'date': this.date });
         this.projectUpdate.update({ 'cost': this.cost });
         this.projectUpdate.update({ 'for': '1' });
-        this.projectUpdate.update({ 'avoid': '9' });
+        this.projectUpdate.update({ 'avoid': this.maxVotingNum - 1 });
         this.projectUpdate.update({ 'against': '0' });
 
         //updating voteStatus
-        this.usersVotingList.update(this.userId, { vote: "for"});
+        this.usersVotingList.update(this.user.id, { vote: "for"});
 
         this.close();
     }
 
-    close() {
+    //===================== close  ============================================================
+
+    private close() 
+    {
         this.showDetailsForm = false;
         this.currentI = -1;
     }
 
+    //===============   scrollToBottom  =========================================================
 
-//======================================================   scrollToBottom  =========================================================
-
-    scrollToBottom(): void {
+    private scrollToBottom(): void 
+    {
         // if(this.firstTimeOfScoller == true)
         // {
-        try {
+        try 
+        {
             this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
         }
-        catch (err) {
-        }
+        catch (err) {}
         this.firstTimeOfScoller = false;
         //  }
         console.log("in scrollToBottom");
 
     }
 
-    viewMore(bol)
+    //===================   viewMore  =========================================================
+
+    private viewMore(bol)
     {
         this.needViewMore = bol;
     }
