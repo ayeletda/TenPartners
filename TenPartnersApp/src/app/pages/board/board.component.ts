7f5ecb6504c4e308b03ce8ac97176d8759972ddc
rachelies <racheli@sacks.family>
2017-06-21 02:00:04 +0300

    re coded incorrect message, stopper improvments

--- TenPartnersApp/src/app/pages/board/board.component.ts
+++ TenPartnersApp/src/app/pages/board/board.component.ts
@@ -3,22 +3,21 @@ import * as firebase from 'firebase/app';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {AfterViewChecked, ElementRef, ViewChild, Component, OnInit} from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
-import {ChangeDetectorRef,Input} from "@angular/core";
+import {ChangeDetectorRef, Input} from "@angular/core";
 import {ServiceService} from '../../service.service';
 
 @Component(
-{
-    selector: 'app-board',
-    templateUrl: './board.component.html',
-    styleUrls: ['./board.component.css']
-})
+    {
+        selector: 'app-board',
+        templateUrl: './board.component.html',
+        styleUrls: ['./board.component.css']
+    })
 
 //====================  Board class  ============================================================
 
-export class BoardComponent implements OnInit 
-{
-    @Input()path;
-    @ViewChild('scrollMe')  myScrollContainer: ElementRef;
+export class BoardComponent implements OnInit {
+    @Input() path;
+    @ViewChild('scrollMe') myScrollContainer: ElementRef;
 
     //user's details
     user;
@@ -37,7 +36,7 @@ export class BoardComponent implements OnInit
     projectUpdate: FirebaseObjectObservable<any>;
 
     //determined according to customer
-    maxVotingNum: number;    
+    maxVotingNum: number;
 
     // pointers of object or list in firebase
     projects: FirebaseListObservable<any>;
@@ -49,16 +48,15 @@ export class BoardComponent implements OnInit
 
     //flags
     doesNeedPop: boolean;
-    noProjects:boolean;
+    noProjects: boolean;
     projectSelected: boolean;
     firstTimeOfScoller: boolean;
-     incorrectValues:boolean;
-    alert:boolean;
+    incorrectValues: boolean;
+    alert: boolean;
 
     //====================  constructor  ============================================================
 
-    constructor( private router: Router, private service: ServiceService, private af: AngularFireDatabase) 
-    {    
+    constructor(private router: Router, private service: ServiceService, private af: AngularFireDatabase) {
         //initializes with defult values
         this.newMessage = '';
         this.savedDate = '';
@@ -67,45 +65,41 @@ export class BoardComponent implements OnInit
         this.projectSelected = false;
         this.firstTimeOfScoller = true;
         this.doesNeedPop = false;
-        this.noProjects= true;
+        this.noProjects = true;
         this.maxVotingNum = 10;
         this.incorrectValues = false;
         this.alert = false;
 
         //function (in servic.component.ts) that returns a pointer to user object that listen to firebase and initializes the variabels: userId, userCommunity, name, email 
-         this.user = this.service.getUser();
+        this.user = this.service.getUser();
 
         //initialize arrays
         this.projects = this.af.list('projects');
 
-        let temp2 = this.projects.subscribe((snapshots) => 
-        {
+        let temp2 = this.projects.subscribe((snapshots) => {
             this.projectsAssociatedCommunities_Arr = [];
             this.projectsValues_Arr = [];
 
-            snapshots.forEach(snapshot => 
-            {
+            snapshots.forEach(snapshot => {
                 this.projectsAssociatedCommunities_Arr.push(this.af.list('projects/' + snapshot.$key + '/associatedCommunities'));
                 this.projectsValues_Arr.push(snapshot);
             });
         });
 
         //pushes subscribe to an array for freeing it (listener to firebase) when login-out
-       this.service.allSubscribe.push(temp2);
+        this.service.allSubscribe.push(temp2);
     }
 
     //===================  ngOnInit  ============================================================
 
-    ngOnInit() 
-    {
+    ngOnInit() {
         this.scrollToBottom()
         this.service.setTitle("Submitted Projects");
     }
 
     //================  saveProjectPath  =========================================================
 
-    saveProjectPath(project, i) 
-    {
+    saveProjectPath(project, i) {
         this.projectPath = 'projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/' + project.$key;
         this.noProjects = false;
         return true;
@@ -113,71 +107,74 @@ export class BoardComponent implements OnInit
 
     //===============  loadProjectDetails  =========================================================
 
-    loadProjectDetails(project, i) 
-    {
+    loadProjectDetails(project, i) {
         this.currentProject = project;
         this.currentI = i;
-        let projectPathH = 'projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/'+project.$key;
-        this.projectUpdate = this.af.object(projectPathH,{preserveSnapshot:true});
+        let projectPathH = 'projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/' + project.$key;
+        this.projectUpdate = this.af.object(projectPathH, {preserveSnapshot: true});
         this.projectSelected = true;
 
         this.cost = project.cost;
         this.date = project.date;
         this.purpose = this.projectsValues_Arr[i].purpose;
         this.description = this.projectsValues_Arr[i].description;
-        this.needViewMore=false;
-        this.usersVotingList = this.af.list('projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/'+project.$key + "/votingList");
+        this.needViewMore = false;
+        this.usersVotingList = this.af.list('projects/' + this.projectsValues_Arr[i].$key + '/associatedCommunities/' + project.$key + "/votingList");
     }
 
     //==================  chooseProject  ============================================================
 
-    chooseProject()
-    {
+    chooseProject() {
         this.doesNeedPop = true;
     }
 
 
-    updateDetails() 
-    {
-         this.close();
-         if(this.date ===null || this.cost<=0 || this.cost == null){
-             this.showIncorrectValues();
-         }
-         else{
-             this.showAlert();
-         }
- 
- 
-     }
-     showIncorrectValues(){
-         this.incorrectValues = true;
-     }
- 
-     submitIncorrectValues(){
-         this.incorrectValues = false;
-         this.showAlert();
-     }
- 
- 
- 
- 
-     showAlert(){
- 
-         this.alert = true;
- 
-     }
-    submitAlert(){
+    updateDetails(event) {
+        if (event.target.parentElement.checkValidity()) {
+            event.target.parentElement.querySelectorAll('input:not([type="submit"]):valid').forEach(function (v) {
+                v.style.border = '';
+                v.nextElementSibling.nextElementSibling.textContent = '';
+            });
+
+            this.close();
+            this.showAlert();
+        } else {
+            event.target.parentElement.querySelectorAll('input:not([type="submit"]):valid').forEach(function (v) {
+                v.style.border = '';
+                v.nextElementSibling.nextElementSibling.textContent = '';
+            });
+
+            event.target.parentElement.querySelectorAll(':invalid').forEach(function (v) {
+                v.style.border = '1px solid red';
+                v.nextElementSibling.nextElementSibling.textContent = v.dataset.msg;
+            });
+        }
+    }
+
+    submitIncorrectValues() {
+        this.incorrectValues = false;
+        this.showAlert();
+    }
+
+
+    showAlert() {
+
+        this.alert = true;
+
+    }
+
+    submitAlert() {
         //updating project's details
-        this.projectUpdate.update({'associatedUser': this.user.id });
-        this.projectUpdate.update({ 'uploudDate': new Date().getTime() });
-        this.projectUpdate.update({ 'date': this.date });
-        this.projectUpdate.update({ 'cost': this.cost });
-        this.projectUpdate.update({ 'for': '1' });
-        this.projectUpdate.update({ 'avoid': this.maxVotingNum - 1 });
-        this.projectUpdate.update({ 'against': '0' });
+        this.projectUpdate.update({'associatedUser': this.user.id});
+        this.projectUpdate.update({'uploudDate': new Date().getTime()});
+        this.projectUpdate.update({'date': this.date});
+        this.projectUpdate.update({'cost': this.cost});
+        this.projectUpdate.update({'for': '1'});
+        this.projectUpdate.update({'avoid': this.maxVotingNum - 1});
+        this.projectUpdate.update({'against': '0'});
 
         //updating voteStatus
-        this.usersVotingList.update(this.user.id, { vote: "for"});
+        this.usersVotingList.update(this.user.id, {vote: "for"});
 
         this.close();
         this.closeAlert();
@@ -185,33 +182,32 @@ export class BoardComponent implements OnInit
 
     //===================== close  ============================================================
 
-    close() 
-    {
+    close() {
         this.doesNeedPop = false;
         this.currentI = 1;
     }
-    closeIncorrectValues(){
-         this.incorrectValues = false;
-         this.doesNeedPop = true;
-     }
- 
-     closeAlert(){
-         this.alert = false;
- 
+
+    closeIncorrectValues() {
+        this.incorrectValues = false;
+        this.doesNeedPop = true;
+    }
+
+    closeAlert() {
+        this.alert = false;
+
 
     }
 
     //===============   scrollToBottom  =========================================================
 
-    scrollToBottom(): void 
-    {
+    scrollToBottom(): void {
         // if(this.firstTimeOfScoller == true)
         // {
-        try 
-        {
+        try {
             this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
         }
-        catch (err) {}
+        catch (err) {
+        }
         this.firstTimeOfScoller = false;
         //  }
         console.log("in scrollToBottom");
@@ -220,8 +216,7 @@ export class BoardComponent implements OnInit
 
     //===================   viewMore  =========================================================
 
-    viewMore(bol)
-    {
+    viewMore(bol) {
         this.needViewMore = bol;
     }
 }
--- TenPartnersApp/src/app/projectForSelect/projectForSelect.component.ts
+++ TenPartnersApp/src/app/projectForSelect/projectForSelect.component.ts
@@ -1,56 +1,66 @@
-
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 import * as firebase from 'firebase/app';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AfterViewChecked, ElementRef, ViewChild, Component, OnInit, EventEmitter } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
-import { ChangeDetectorRef, Input, Output } from "@angular/core";
+import {AngularFireAuth} from 'angularfire2/auth';
+import {AfterViewChecked, ElementRef, ViewChild, Component, OnInit, EventEmitter} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
+import {ChangeDetectorRef, Input, Output} from "@angular/core";
 
 @Component(
-{
-  selector: 'app-projectForSelect',
-  templateUrl: './projectForSelect.component.html',
-  styleUrls: ['./projectForSelect.component.css']
-})
+    {
+        selector: 'app-projectForSelect',
+        templateUrl: './projectForSelect.component.html',
+        styleUrls: ['./projectForSelect.component.css']
+    })
 
-export class ProjectForSelectComponent implements OnInit, AfterViewChecked 
-{
-  @Input() item;
-  @Output() voteChoose: EventEmitter<any> = new EventEmitter();
+export class ProjectForSelectComponent implements OnInit, AfterViewChecked {
+    @Input() item;
+    @Input() project;
+    @Output() voteChoose: EventEmitter<any> = new EventEmitter();
 
-  @ViewChild('forVal') private forVal: any;
-  @ViewChild('avoidVal') private avoidVal: any;
-  @ViewChild('againstVal') private againstVal: any;
+    @ViewChild('forVal') private forVal: any;
+    @ViewChild('avoidVal') private avoidVal: any;
+    @ViewChild('againstVal') private againstVal: any;
 
-  votingFor: boolean;
-  votingFirstTime: boolean; // saves if it the first time that person voting (true) or not (false)
-  private projects: FirebaseListObservable<any>;
-  private pointerToProjectInAF: any;
-  pointerToProjectObjectInAF: any;
-  projectName: string;
-  projectUplodeDate: Date;
-
-
-  constructor(private router: Router, private af: AngularFireDatabase) {
-
-    this.projects = this.af.list('projects');
-    (<any>$(".stopper")).each(function (k, v) {
-     (<any>$(v)).countdown(v.dataset.date, function(event) {
-         $(this).text(event.strftime('%D :%H:%M:%S'));
-       });
-     });
-  }
+    votingFor: boolean;
+    votingFirstTime: boolean; // saves if it the first time that person voting (true) or not (false)
+    private projects: FirebaseListObservable<any>;
+    private pointerToProjectInAF: any;
+    pointerToProjectObjectInAF: any;
+    projectName: string;
+    projectUplodeDate: Date;
 
 
-  ngOnInit() {
-    this.pointerToProjectInAF = this.af.list(this.item); // item is a path
-    this.pointerToProjectObjectInAF = this.af.object(this.item, { preserveSnapshot: true });
-    this.projectName = this.pointerToProjectInAF.$ref.path.o[1];
+    constructor(private router: Router, private af: AngularFireDatabase) {
 
-  }
+        this.projects = this.af.list('projects');
+        (<any>$(".stopper")).each(function (k, v) {
+            if (v.textContent.trim().match(/^\d{13}$/) !== null) {
+                //let date = new Date(parseInt(v.textContent) + (14 * 60 * 60 * 24 * 1000));
+                let date = new Date(parseInt(v.textContent));
+
+                (<any>$(v)).countdown(date, function (event) {
+                    $(this).text(event.strftime('%D:%H:%M:%S'));
+                }).on('finish.countdown', function () {
+                   //TODO: timer done
+                });
+            } else {
+                console.log(v.textContent.trim().match(/^\d{13}$/));
+            }
+        });
+    }
 
 
-  ngAfterViewChecked() {
+    ngOnInit() {
+        this.pointerToProjectInAF = this.af.list(this.item); // item is a path
+        this.pointerToProjectObjectInAF = this.af.object(this.item, {preserveSnapshot: true});
+        this.projectName = this.pointerToProjectInAF.$ref.path.o[1];
 
-  }
+        this.projectUplodeDate = this.project.uploudDate;
+
+    }
+
+
+    ngAfterViewChecked() {
+
+    }
 }
